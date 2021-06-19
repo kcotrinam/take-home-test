e14f821e889e94dc267f71e8705f720eea1691cf
Kevin Cotrina <kcotrinam92@gmail.com>
2021-06-19 13:38:53 -0500

    Add async actions to fetch commits history

--- src/reducers/historyReducer.js
+++ src/reducers/historyReducer.js
@@ -1,3 +1,4 @@
+import * as types from '../actions/actionTypes'
 const INITIAL_STATE = {
   loading: false,
   history: [],
@@ -6,6 +7,23 @@ const INITIAL_STATE = {
 
 const historyReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case types.FETCH_HISTORY_REQUEST:
+      return {
+        ...state,
+        loading: true
+      }
+    case types.FETCH_HISTORY_SUCESS:
+      return {
+        ...state,
+        loading: false,
+        history: action.payload
+      }
+    case types.FETCH_HISTORY_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload
+      }
     default:
       return state
   }
