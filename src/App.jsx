cd7960723f3e0be37a99dcdd519d3c721c45b88d
Kevin Cotrina <kcotrinam92@gmail.com>
2021-06-19 16:57:04 -0500

    Add Timeline container component inside App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Timeline from './containers/Timeline'
 
 const App = () => {
   return (
     <>
-      <div>Hello world!</div>
+      <Timeline />
     </>
   )
 }
