f8b2e70eabc14d70ef43a251d3adc6c192d614e4
Kevin Cotrina <kcotrinam92@gmail.com>
2021-06-21 01:41:29 -0500

    Refactor Header compnoent to render filter input

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import { Box, Text } from '@chakra-ui/react'
+import { Box, Grid, Text } from '@chakra-ui/react'
+import FilterInput from '../components/FilterInput'
 
 const Header = () => {
   return (
     <>
       <Box bgColor='blue.400'>
-        <Text fontSize='5xl' align='center'>
-          Header
-        </Text>
+        <Grid templateColumns='1fr 1fr'>
+          <Text fontSize='5xl' align='center' color='gray.100'>
+            TTH
+          </Text>
+          <FilterInput />
+        </Grid>
       </Box>
     </>
   )
